24ea10aa6224ff1f914d82adc04e59aaf9d70380
Naohisa Yokoyama <nhsykym@gmail.com>
2020-06-14 18:00:51 +0900

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by 横山尚久 on 2020/06/14.
+//  Copyright © 2020 横山尚久. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
